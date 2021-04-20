bda870ccc21d4585d68457f183452e09a50c113e
Hannah-the-dev <hannah.the.dev@gmail.com>
2021-04-20 20:44:43 +0900

    finalized

--- src/receipts/Receipt2.java
+++ src/receipts/Receipt2.java
@@ -9,41 +9,41 @@ public class Receipt2 {		//daiso
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		// TODO Auto-generated method stub
-		DecimalFormat k21_format = new DecimalFormat("###,###,###");
-		Calendar k21_cal1 = Calendar.getInstance();
-		SimpleDateFormat k21_sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		SimpleDateFormat k21_sdf1 = new SimpleDateFormat("MM월dd일");
-		k21_cal1.add(Calendar.DATE, 14);
-		
-//		String k21_item1 = "3중필터 일회용마스크 50매 대형 블랙 화이트";
-//		String k21_itemCode1 = "1031615";
-//		int k21_itemPrice1 = 3000;
-//		int k21_itemQty1 = 1;
-//
-//		String k21_item2 = "라탄 패턴 바구니 바스켓";
-//		String k21_itemCode2 = "11008152";
-//		int k21_itemPrice2 = 1000;
-//		int k21_itemQty2 = 2;
-//		
-//		String k21_item3 = "센스 다용도 저안트레이 24cm 2호 1개";
-//		String k21_itemCode3 = "1020800";
-//		int k21_itemPrice3 = 1000;
-//		int k21_itemQty3 = 1;
-
-		String k21_item1 = "퓨어메딕 비말차단용마스크(최고급형)";
+		String k21_item1 = "3중필터 일회용마스크 50매 대형 블랙 화이트";
 		String k21_itemCode1 = "1031615";
 		int k21_itemPrice1 = 3000;
 		int k21_itemQty1 = 1;
-		
-		String k21_item2 = "슬라이드식 명찰(가로형)         ";
+
+		String k21_item2 = "라탄 패턴 바구니";
 		String k21_itemCode2 = "11008152";
 		int k21_itemPrice2 = 1000;
 		int k21_itemQty2 = 2;
 		
-		String k21_item3 = "매직흡착 인테리어후크(알루미늄타입)";
+		String k21_item3 = "센스 다용도 저안트레이 24cm 2호 1개";
 		String k21_itemCode3 = "1020800";
 		int k21_itemPrice3 = 1000;
 		int k21_itemQty3 = 1;
+
+//		String k21_item1 = "퓨어메딕 비말차단용마스크(최고급형)";
+//		String k21_itemCode1 = "1031615";
+//		int k21_itemPrice1 = 3000;
+//		int k21_itemQty1 = 1;
+//		
+//		String k21_item2 = "슬라이드식 명찰(가로형)         ";
+//		String k21_itemCode2 = "11008152";
+//		int k21_itemPrice2 = 1000;
+//		int k21_itemQty2 = 2;
+//		
+//		String k21_item3 = "매직흡착 인테리어후크(알루미늄타입)";
+//		String k21_itemCode3 = "1020800";
+//		int k21_itemPrice3 = 1000;
+//		int k21_itemQty3 = 1;
+		
+		DecimalFormat k21_format = new DecimalFormat("###,###,###");			// 금액 등 수 표시 형식
+		Calendar k21_cal1 = Calendar.getInstance();								// 현재 시간 저장(+14일 용)
+		k21_cal1.add(Calendar.DATE, 14);										// 14일 더해 교환, 환불 가능일 계산
+		SimpleDateFormat k21_sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");	// 시간 출력 포맷
+		SimpleDateFormat k21_sdf1 = new SimpleDateFormat("MM월dd일");			// 일자 출력 포맷
 		
 		System.out.printf("%25s\n", "\"국민가게, 다이소\"");
 		System.out.printf("%.45s\n", "(주)아성다이소_분당서현점");
@@ -52,57 +52,58 @@ public class Receipt2 {		//daiso
 		System.out.printf("%.45s\n", "대표:박정부, 신호섭 213-81-52063");
 		
 		String k21_address = "경기도 성남시 분당구 분당로53번길 11 (서현동)";
-		System.out.printf("매장:%.26s\n", k21_address);
-		System.out.printf("%.25s\n", k21_address.substring(26));
+		System.out.printf("매장:%.26s\n", k21_address);							// 매장 주소 앞 26자리 출력
+		System.out.printf("%.26s\n", k21_address.substring(26));				// 그 이후 자리 출력(최대 26자리)
 		
 		System.out.printf("================================================\n");
 		System.out.printf("         소비자중심경영(CCM) 인증기업           \n");
 		System.out.printf("       ISO 9001 품질경영시스템 인증기업         \n");
 		System.out.printf("================================================\n");
-		System.out.printf("         교환//환불 14일(%s)이내,          \n", k21_sdf1.format(k21_cal1.getTime()));
+		System.out.printf("         교환//환불 14일(%s)이내,          \n", k21_sdf1.format(k21_cal1.getTime()));	//환불가능일
 		System.out.printf("(전자)영수증, 결제카드 지참 후 구입매장에서 가능\n");
 		System.out.printf("      포장/가격 택 훼손시 교환/환불 불가        \n");
 		System.out.printf("        체크카드 취소 시 최대 7일 소요          \n");
 		System.out.printf("================================================\n");
-		Calendar k21_cal = Calendar.getInstance();
-		System.out.printf("[POS %s] %34s\n", "1058231", k21_sdf.format(k21_cal.getTime()));
+		Calendar k21_cal = Calendar.getInstance();								// 현재 시간 다시 받기
+		System.out.printf("[POS %s] %34s\n", "1058231", k21_sdf.format(k21_cal.getTime()));	// 매출 시간, 포스번호 출력
 		System.out.printf("================================================\n");
-		int k21_itemAmount1 = k21_itemPrice1 * k21_itemQty1;
-		int k21_itemAmount2 = k21_itemPrice2 * k21_itemQty2;
-		int k21_itemAmount3 = k21_itemPrice3 * k21_itemQty3;
+		int k21_itemAmount1 = k21_itemPrice1 * k21_itemQty1;					// 1번 아이템 금액 계산
+		int k21_itemAmount2 = k21_itemPrice2 * k21_itemQty2;					// 2번 아이템 금액 계산
+		int k21_itemAmount3 = k21_itemPrice3 * k21_itemQty3;					// 3번 아이템 금액 계산
 		
-		gettingBytes by = new gettingBytes();
-		String item1Str = by.gettingSpaces(k21_item1, 14);
-		String item2Str = by.gettingSpaces(k21_item2, 14);
-		String item3Str = by.gettingSpaces(k21_item3, 14);
+		gettingBytes by = new gettingBytes();									// gettingBytes 클래스 사용선언
+		String item1Str = by.gettingSpaces(k21_item1, 14);						// 14번째 자리에서 자른 문자열1
+		String item2Str = by.gettingSpaces(k21_item2, 14);						// 14번째 자리에서 자른 문자열2						
+		String item3Str = by.gettingSpaces(k21_item3, 14);						// 14번째 자리에서 자른 문자열3
 	
+		// 아이템1, 단가1, 수량1, 가격 1 출력
 		System.out.printf("%s %6s %3d %8s\n", item1Str, k21_format.format(k21_itemPrice1), 
 				k21_itemQty1, k21_format.format(k21_itemAmount1));
-		System.out.printf("[%s]\n", k21_itemCode1);
-		
+		System.out.printf("[%s]\n", k21_itemCode1);			// 품목번호1 출력
+		// 아이템2, 단가2, 수량2, 가격 2 출력
 		System.out.printf("%s %6s %3d %8s\n", item2Str, k21_format.format(k21_itemPrice2), 
 				k21_itemQty2, k21_format.format(k21_itemAmount2));
-		System.out.printf("[%s]\n", k21_itemCode2);
-		
+		System.out.printf("[%s]\n", k21_itemCode2);			// 품목번호2 출력
+		// 아이템3, 단가3, 수량3, 가격 3 출력
 		System.out.printf("%s %6s %3d %8s\n", item3Str, k21_format.format(k21_itemPrice3), 
 				k21_itemQty3, k21_format.format(k21_itemAmount3));
-		System.out.printf("[%s]\n", k21_itemCode3);
+		System.out.printf("[%s]\n", k21_itemCode3);			//품목번호3 출력
 		
-		int k21_amountSum = k21_itemAmount1+k21_itemAmount2+k21_itemAmount3;
-		int k21_beforeTax = (int) Math.ceil(k21_amountSum/1.1);
-		System.out.printf("%16s %27s\n", "과세합계", k21_format.format(k21_beforeTax));
-		System.out.printf("%17s %27s\n", "부가세", k21_format.format(k21_amountSum-k21_beforeTax));
+		int k21_amountSum = k21_itemAmount1+k21_itemAmount2+k21_itemAmount3;		// 아이템 3개 금액 총합
+		int k21_beforeTax = (int) Math.ceil(k21_amountSum/1.1);						// 과세금액 계산(소수점 올림)
+		System.out.printf("%16s %27s\n", "과세합계", k21_format.format(k21_beforeTax));		// 과세 합계 출력
+		System.out.printf("%17s %27s\n", "부가세", k21_format.format(k21_amountSum-k21_beforeTax));	// 부가세=매출액-과세합계
 		System.out.printf("------------------------------------------------\n");
-		System.out.printf("%s %39s\n", "판매합계", k21_format.format(k21_amountSum));
+		System.out.printf("%s %39s\n", "판매합계", k21_format.format(k21_amountSum));		// 총 매출액
 		System.out.printf("================================================\n");
-		String k21_credit = "1234567890101112";
-		System.out.printf("%s %39s\n", "신용카드", k21_format.format(k21_amountSum));
+		String k21_credit = "1234567890101112";										// 임의의 카드번호
+		System.out.printf("%s %39s\n", "신용카드", k21_format.format(k21_amountSum));	// 카드 매출액
 		System.out.printf("------------------------------------------------\n");
-		System.out.printf("%s %29.6s**********\n", "우리카드", k21_credit);
-		String k21_approval = "승인금액 " + k21_format.format(k21_amountSum);
-		System.out.printf("승인번호 %s(0) %23s\n", "77982843", k21_approval);
+		System.out.printf("%s %29.6s**********\n", "우리카드", k21_credit);				// 카드 6자리 이후 *출력
+		String k21_approval = "승인금액 " + k21_format.format(k21_amountSum);			// 총 승인금액 
+		System.out.printf("승인번호 %s(0) %23s\n", "77982843", k21_approval);			// 승인 금액 출력
 		System.out.printf("================================================\n");
-		System.out.printf("%27s 분당서현점\n", k21_sdf.format(k21_cal.getTime()));
+		System.out.printf("%27s 분당서현점\n", k21_sdf.format(k21_cal.getTime()));		// 매출 시간 출력
 		System.out.printf("상품 및 기타 문의 : 1522-4400\n");
 		System.out.printf("멤버십 및 샵다이소 관련 문의 : 1599-2211\n");
 		System.out.printf("              2112820610158231\n");
--- src/receipts/gettingBytes.java
+++ src/receipts/gettingBytes.java
@@ -1,12 +1,13 @@
 package receipts;
 
-public class gettingBytes {				//bytes: 보여주고 싶은 문자열의 너비
+public class gettingBytes {				
+	// 길이 입력받은 길이만큼 자르는 매서드	       //bytes: 보여주고 싶은 문자열의 너비
 	public String gettingSpaces(String k21_words, int k21_bytes) throws StringIndexOutOfBoundsException{
-		k21_words += "                  ";			// words 문자열이 짧을 경우 에러 방지하기 위해 공백 추가
-		String[] k21_charactors = k21_words.split(""); // words에 포함된 문자로 배열 구성 
+		k21_words += "                  ";				// words 문자열이 짧을 경우 에러 방지하기 위해 공백 추가
+		String[] k21_charactors = k21_words.split(""); 	// words에 포함된 문자로 배열 구성 
 		int k21_width = 0;								// 보이는 너비(전자=2, 반자1)
 		int k21_cutHere = 0;							// 해당 너비에서 글자수(전자, 반자 모두 1)
-		for (String k21_charactor : k21_charactors) {  // 각 글자가
+		for (String k21_charactor : k21_charactors) {  	// 각 글자가
 			if (k21_charactor.getBytes().length == 1) {	// 1바이트 문자일 경우
 				k21_width ++ ;							// width는 1추가
 				k21_cutHere ++;							// cutHere는 1추가
@@ -14,20 +15,22 @@ public class gettingBytes {				//bytes: 보여주고 싶은 문자열의 너비
 				k21_width +=2;							// width 는 2추가
 				k21_cutHere ++;							// cutHere는 1추가
 			}
-			if (k21_width >= k21_bytes * 2) {		// width가 보여주고 싶은 문자열의 너비*2와 같거나 커지면
+			if (k21_width >= k21_bytes * 2) {			// width가 보여주고 싶은 문자열의 너비*2와 같거나 커지면
 				break;									// break, 
 				}
 			}
 		
-		String k21_answer = k21_words.substring(0, k21_cutHere);
+		String k21_answer = k21_words.substring(0, k21_cutHere);	//cutHere 변수에서 자름
 		return k21_answer;
 	}
-	public String gettingAnonymous(String k21_original, int start, int end) {
+
+	// 이름, 카드번호 등 개인정보 마스킹
+	public String gettingAnonymous(String k21_original, int start, int end) {		
 		int k21_len = k21_original.length();				// 길이 변수 생성
 		String k21_answer = k21_original.charAt(0)+"";		// 첫글자로 리턴문구 첫자 초기화
 		
-		for (int k21_i = 1; k21_i < start-1; k21_i++) {		// start 전까지는 원 문자 그대로 저장
-			k21_answer += k21_original.charAt(k21_i)+"";	
+		for (int k21_i = 1; k21_i < start-1; k21_i++) {		// start 전까지는
+			k21_answer += k21_original.charAt(k21_i)+"";	// 원 문자 그대로 저장
 		}
 		for (int k21_i = start; k21_i < end+1; k21_i++) {	// start부터 end 까지는 * 저장
 			k21_answer += "*";	
